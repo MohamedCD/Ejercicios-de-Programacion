2a98f5a2b6c8247e5554a5ef534d87ae4d07b08e
Mohamed Chahdi Dkikar <45308318+MohamedCD@users.noreply.github.com>
2019-10-08 09:09:52 +0200

    Add files via upload

---
+++ Programacion/Primer Trimestre/Ejercicios 5 - Bucles/Ejercicio03.java
@@ -0,0 +1,14 @@
+public class Ejercicio03{
+	public static void main(String[] args) {
+
+		int i = 1;
+
+		do{
+			if (i%5 == 0) {
+				System.out.println(i);
+			}
+			i++;
+		}
+		while (i <= 100);
+	}
+}
