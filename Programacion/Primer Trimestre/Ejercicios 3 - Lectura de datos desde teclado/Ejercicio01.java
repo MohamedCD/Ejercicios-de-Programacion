c1404f046311967df1b0ea240ece58f4d424ca67
Mohamed Chahdi Dkikar <45308318+MohamedCD@users.noreply.github.com>
2019-10-08 09:17:43 +0200

    Add files via upload

---
+++ Programacion/Primer Trimestre/Ejercicios 3 - Lectura de datos desde teclado/Ejercicio01.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Ejercicio01{
+  public static void main(String[] args){
+    
+  Scanner sc = new Scanner(System.in);  
+  
+    int num1, num2, resultado;
+    
+    System.out.println("Introduce el primer número");
+    num1 = sc.nextInt();
+  
+    System.out.println("Introduce el segundo número");
+    num2 = sc.nextInt();
+    
+    resultado = num1 * num2;
+    
+    System.out.println("El resultado de la multiplicación es: "+resultado);
+  
+  }
+}
---
+++ Programacion/Primer Trimestre/Ejercicios 3 - Lectura de datos desde teclado/Ejercicio07.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Ejercicio07{
+  public static void main(String[] args){
+   
+    Scanner sc = new Scanner(System.in);
+    
+    System.out.println("Introduce la base imponible: ");
+    float base = sc.nextFloat();
+    
+    float total = base * 1.21f;
+    
+    System.out.println("El total de la factura es de: " + total);
+  }
+}
